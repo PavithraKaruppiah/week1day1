941cbdb7c5c8bfb92239e6923f658964d8e3ca7d
PavithraKaruppiah <101961073+PavithraKaruppiah@users.noreply.github.com>
2022-03-21 15:30:30 +0300

    Add files via upload

---
+++ NegativePositive.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class NegativePositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int Neg= -40;
+
+if(Neg<0) 
+{
+	int i=Neg*-1;
+	System.out.println(i);
+	System.out.println("The number -40 is converted to 40");	
+}
+
+else if (Neg>0)
+{
+	System.out.println("The number is Already Positive");
+	}
+	}
+
+}
