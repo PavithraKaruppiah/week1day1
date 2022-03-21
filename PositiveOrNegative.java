494532185ae43c5bfbe07e3ce8ccf680e13fb2e2
PavithraKaruppiah <101961073+PavithraKaruppiah@users.noreply.github.com>
2022-03-21 15:26:06 +0300

    Add files via upload

---
+++ PositiveOrNegative.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Pos=35;
+		if(Pos>0) {
+			
+			System.out.println("Positive Number");
+		}
+		
+		else if(Pos<0) {
+			System.out.println("Negative Number");
+		}
+	}
+
+}
