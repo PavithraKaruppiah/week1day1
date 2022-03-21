702d993628e26a38df43d56501e4ff5e2c7cd0d4
PavithraKaruppiah <101961073+PavithraKaruppiah@users.noreply.github.com>
2022-03-21 15:25:08 +0300

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=5;
+int fact=1;
+
+for(int j=1;j<=i;j++) {
+	fact = fact *j;
+}
+{
+	System.out.println(fact);
+	
+}
+	}
+
+}
